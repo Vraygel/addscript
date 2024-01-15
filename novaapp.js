3758391990e3460e287d196e6f0504e6f82862f7
Владимир Александрович Райгель <vraygel@mail.ru>
2024-01-15 15:12:29 +0500

    первая версия

---
+++ novaapp.js
@@ -0,0 +1,221 @@
+let programming_button = document.querySelector('.programming_button') // кнопка "програмирование скрита"
+let wrapper_programming = document.querySelector('.wrapper_programming') // контейнер програмирование скрипта
+let list_button = document.querySelector('.list_button') // кнопка "вопросы и ответы"
+let wrapper_list = document.querySelector('.wrapper_list') // контейнер список вопросов и ответов
+let work_button = document.querySelector('.work_button') // кнопка "Показать скрипт"
+let wrapper_work = document.querySelector('.wrapper_work') // контейнер работы со скриптом
+let addDinamic = document.querySelector('.addDinamic')  // кнопка "добавить динамичекое поле"
+let dinamic_div = document.querySelector('.dinamic_div') // контейнер динимических полей добавление
+let dinamic_div_prog = document.querySelector('.dinamic_div_prog') // контейнер динимических полей вставки
+let dinamic_div_hide = document.querySelectorAll('.dinamic_div_hide') // кнопка "скрыть" динамичекое поле
+let myTxtArea = document.querySelector('.myTxtArea ') // textarea редактирования вопроса
+let myTxtArea_div = document.querySelector('.myTxtArea_div') // div визуально представляющий вопрос
+let insertDinamic = document.querySelector('.insertDinamic') // кнопка "Вставить Динамическое поле"
+let dinamic_save = document.querySelector('.dinamic_save') // кнопка "Сохранить" Динамическое поле
+let dinamic_list = document.querySelectorAll('.dinamic_list') // контейнер списка динамических полей добавления
+// let dinamic_list_prog = document.querySelector('.dinamic_list_prog') // контейнеры списка динамических полей втавки
+
+
+let question_answer = [] // массив содержащий все объекты вопросов и ответов
+let dinamicListArr = [] // массив содержащий все объекты динамических полей
+
+programming_button.addEventListener('click', () => programming_buttonClick()) // кнопка "програмирование скрита" событие клик
+
+function programming_buttonClick() {
+	wrapper_programming.style.display = 'block'
+	wrapper_work.style.display = 'none'
+	wrapper_list.style.display = 'none'
+}
+
+list_button.addEventListener('click', () => list_buttonClick()) // кнопка "вопросы и ответы" событие клик
+
+function list_buttonClick() {
+	wrapper_list.style.display = 'flex'
+	wrapper_work.style.display = 'none'
+	wrapper_programming.style.display = 'none'
+}
+
+work_button.addEventListener('click', () => work_buttonClick()) // кнопка "Показать скрипт" событие клик
+
+function work_buttonClick() {
+	wrapper_work.style.display = 'block'
+	wrapper_list.style.display = 'none'
+	wrapper_programming.style.display = 'none'
+}
+
+for (const iterator of dinamic_div_hide) { // для всех кнопок "скрыть" динамичекое поле
+	iterator.addEventListener('click', (event) => { // событие клик
+		dinamic_div.style.display = 'none'
+		dinamic_div_prog.style.display = 'none'
+	})
+}
+
+myTxtArea.addEventListener('input', (event) => { // текст из myTxtArea транслируется в myTxtArea_div
+	console.log(myTxtArea.value);
+	myTxtArea_div.textContent = myTxtArea.value
+})
+
+insertDinamic.addEventListener('click', (event) => { // кнопка "вставить динамичекое поле" событие клик
+	event.preventDefault()
+	dinamic_div_prog.style.display = 'block'
+})
+
+
+
+
+
+
+
+
+
+
+
+list_buttonClick()
+
+addDinamic.addEventListener('click', (event) => addDinamicClick()) // кнопка "добавить динамичекое поле" событие клик
+
+
+
+
+
+
+function addDinamicClick() {
+	dinamic_div.style.display = 'block' // контейнер динимических полей добавление
+
+	dinamicListFunc('dinamic_dell', 'Удалить') // отображение списка динамических полей с кнопкой "удалить" динамическое поле
+}
+
+
+
+
+
+
+function dinamicListFunc(buttonClass, buttonValue) { // отображение списка динамических полей 
+
+	for (const iterator of dinamic_list) { // контейнер списка динамических полей добавления
+		iterator.innerHTML = '' // очищаем
+	}
+
+	if (dinamicListArrInlocalStorage()) { // если есть сохраненный dinamicListArr и он не пустой получаем dinamicListArr
+
+		dinamicListArr.forEach(element => { // для каждого объекта (элемента) динамического поля
+
+			for (const iterator of dinamic_list) { // для каждого dinamic_lista
+				iterator.innerHTML += `
+					<div class='dinamic_list_item'>
+						Системное имя: ${element.name};
+						Подсказка: ${element.placeholder};
+						На странице: <input class="dinamic_input" id="${element.id}" name="${element.name}" type="text" placeholder="${element.placeholder}">
+						<button class="${buttonClass}" id="${element.id}">${buttonValue}</button>
+						<br>
+					</div>
+					`
+			}
+		});
+
+		// dinamic_add = []
+		// dinamic_add = document.querySelectorAll('.dinamic_add')
+		// dinamicAddClick(dinamic_add) 
+		dinamicDellFunc() // запускаем функцию удаления динамичеого поля
+	}
+	else { // иначе `Динамические поля не созданы`
+		for (const iterator of dinamic_list) {
+			iterator.innerHTML = `Динамические поля не созданы`
+		}
+
+	}
+}
+
+dinamic_save.addEventListener('click', () => dinamic_saveClick()) // кнопка "Сохранить" Динамическое поле событие "Клик"
+
+function dinamic_saveClick() {
+	
+	let dinamicListObj = {} // объект содержащий информацию о динамическом поле
+	let id = getId(length = 16) // генерация случайного номера ID
+
+	let name = document.querySelector('input[name="name"]') // ищем input с атрубутом name="name"
+	let placeholder = document.querySelector('input[name="placeholder"]')// ищем input с атрубутом name="placeholder"
+
+	dinamicListObj.id = id // записываем в объект динамичекого поля id
+	dinamicListObj.name = name.value // записываем в объект динамичекого поля name 
+	dinamicListObj.placeholder = placeholder.value // записываем в объект динамичекого поля placeholder
+
+	dinamicListArr.push(dinamicListObj) // добавляем в массив динамических полей объект нового динамического поля
+
+	let json = JSON.stringify(dinamicListArr) // создаем JSON из массива динамических полей
+	localStorage.setItem('dinamicListArr', json) // сохраняем в localStorage JSON массива динамических полей
+
+	name.value = '' // очищаем input name
+	placeholder.value = '' // очищаем input placeholder
+
+	dinamicListFunc('dinamic_dell', 'Удалить')
+	dinamicDellFunc()
+}
+
+
+function dinamicDellFunc() { // функция удаления динамичеого поля
+	let dinamic_dell = document.querySelectorAll('.dinamic_dell') // ищем все кнопки "Удалить" динамическое поле
+
+	dinamicListArrInlocalStorage()// проверяем существование сохраненного в localStorage массива dinamicListArr
+
+	for (const iterator of dinamic_dell) { // для каждой найденой кнопки
+
+		iterator.addEventListener('click', (e) => { // событие клик
+			let id = e.target.getAttribute('id') // получаем id кнопки
+			e.target.parentElement.remove() //удаляем кнопку
+			dinamicListArr.forEach((element, index, arr) => { // для каждого объекта (элемента) динамического поля
+				if (element.id == id) { // если id элемента равно с id кнопки
+					arr.splice(index, 1) // удаляем этот элемент из массива dinamicListArr
+				}
+			});
+			let json = JSON.stringify(dinamicListArr); // создаем JSON массива dinamicListArr
+			localStorage.setItem('dinamicListArr', json) // записываем JSON массива dinamicListArr в localStorage
+			dinamicListFunc('dinamic_dell', 'Удалить') // запускаем отображение списка динамических полей с кнопкой удалить
+		})
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function dinamicListArrInlocalStorage() { // проверяем существование сохраненного в localStorage массива dinamicListArr
+
+	if (localStorage.getItem('dinamicListArr') && localStorage.getItem('dinamicListArr').length != 2) { // если есть сохраненный dinamicListArr и он не пустой
+		let json = localStorage.getItem('dinamicListArr') // получаем JSON dinamicListArr
+		dinamicListArr = JSON.parse(json) // парсим JSON dinamicListArr
+		return true
+	}
+	else {
+		return false
+	}
+}
+
+
+
+
+function getId(length = 16) { // генерация случайного номера ID
+	let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+	let res = '';
+
+	for (let i = 0; i < length; i++) {
+		res += chars[Math.floor(Math.random() * chars.length)];
+	}
+	return res;
+}
---
+++ perevod.js
@@ -0,0 +1,363 @@
+let wrap = document.querySelector('.wrap')
+let butt = document.querySelector('.butt')
+let myTxtArea = document.querySelector('.myTxtArea')
+let input_color = document.querySelector('.input_color')
+let buten_text = document.querySelector('.buten_text')
+let myTxtArea_div = document.querySelector('.myTxtArea_div')
+let button_input_color = document.querySelector('.button_input_color')
+let mceContainer = document.querySelector('.mce-container')
+let clouse = document.querySelector('.clouse')
+let addDinamic = document.querySelector('.addDinamic')
+let dinamic_save = document.querySelector('.dinamic_save')
+let dinamic_list = document.querySelector('.dinamic_list')
+let dinamic_div = document.querySelector('.dinamic_div')
+let dinamic_div_hide = document.querySelector('.dinamic_div_hide')
+let wrapp_dinamic_add = document.querySelector('.wrapp_dinamic_add')
+let mini_but_color = document.querySelectorAll('.mini_but_color')
+let mini_div_color = document.querySelectorAll('.mini_div_color')
+let strong = document.querySelector(".format-strong");
+let em = document.querySelector(".format-em");
+let link = document.querySelector(".format-link");
+let code = document.querySelector(".format-code");
+let dinamic_add
+
+function addVariable() {
+  wrap = document.querySelector('.wrap')
+  butt = document.querySelector('.butt')
+  myTxtArea = document.querySelector('.myTxtArea')
+  input_color = document.querySelector('.input_color')
+  buten_text = document.querySelector('.buten_text')
+  myTxtArea_div = document.querySelector('.myTxtArea_div')
+  button_input_color = document.querySelector('.button_input_color')
+  mceContainer = document.querySelector('.mce-container')
+  clouse = document.querySelector('.clouse')
+  addDinamic = document.querySelector('.addDinamic')
+  dinamic_save = document.querySelector('.dinamic_save')
+  dinamic_list = document.querySelector('.dinamic_list')
+  dinamic_div = document.querySelector('.dinamic_div')
+  dinamic_div_hide = document.querySelector('.dinamic_div_hide')
+  wrapp_dinamic_add = document.querySelector('.wrapp_dinamic_add')
+  mini_but_color = document.querySelectorAll('.mini_but_color')
+  mini_div_color = document.querySelectorAll('.mini_div_color')
+  strong = document.querySelector(".format-strong");
+  em = document.querySelector(".format-em");
+  link = document.querySelector(".format-link");
+  code = document.querySelector(".format-code");
+	programming_button = document.querySelector('.programming_button')
+
+	buttonEventListener()
+}
+
+
+
+
+
+
+let dinamicListArr = []
+
+
+
+
+
+for (const item of mini_but_color) {
+  item.addEventListener('click', (e) => {
+    console.log(item.getAttribute('data-mce-color'));
+    e.preventDefault()
+    var range = window.getSelection().getRangeAt(0);
+    var selectionContents = range.extractContents();
+    var span = document.createElement("span");
+    span.style.color = `${item.getAttribute('data-mce-color')}`;
+    console.log(span);
+    span.appendChild(selectionContents);
+    range.insertNode(span);
+    mceContainer.style.display = 'none';
+
+  })
+}
+
+clouse.addEventListener('click', (event) => {
+  mceContainer.style.display = 'none';
+})
+
+function insertURL() {
+  const newURL = prompt("Enter the full URL for the link");
+  if (newURL) {
+    insert(`<a href="${newURL}">`, "</a>");
+  } else {
+    document.myForm.myTxtArea.focus();
+  }
+}
+
+let input = `<input type="text" class="input" id="" placeholder="Имя клиента"></input>`
+
+
+
+
+buttonEventListener()
+
+function buttonEventListener() {
+  button_input_color.addEventListener('click', (e) => {
+    e.preventDefault()
+    mceContainer.style.display = 'block';
+  
+    let textArea = document.querySelector('.myTxtArea');
+    let selectionStart = textArea.selectionStart;
+    console.log(selectionStart);
+    let selectionEnd = textArea.selectionEnd;
+    let oldText = textArea.value;
+  
+    let prefix = oldText.substring(0, selectionStart);
+    let inserted = oldText.substring(selectionStart, selectionEnd);
+    console.log(inserted);
+    let suffix = oldText.substring(selectionEnd);
+    textArea.value = `${prefix}${inserted}${suffix}`;
+  
+  })
+
+  em.addEventListener("click", (e) => {
+    e.preventDefault()
+    var range = window.getSelection().getRangeAt(0);
+    var selectionContents = range.extractContents();
+    var span = document.createElement("span");
+    span.style.fontStyle = 'italic';
+    span.appendChild(selectionContents);
+    range.insertNode(span);
+  });
+
+  strong.addEventListener("click", (e) => {
+    e.preventDefault()
+    var range = window.getSelection().getRangeAt(0);
+    var selectionContents = range.extractContents();
+    var span = document.createElement("span");
+    span.style.fontWeight = "600";
+    span.appendChild(selectionContents);
+    range.insertNode(span);
+  });
+
+  code.addEventListener("click", (e) => {
+
+    e.preventDefault()
+    wrapp_dinamic_add.style.display = 'none'
+    dinamic_list.innerHTML = ''
+    // debugger
+    dinamicListFunc('dinamic_add', 'Добавить') 
+  
+    
+    
+    
+  });
+}
+
+
+function dinamicAddClick(dinamic_add) {
+  for (const iterator of dinamic_add) {
+    // console.log(dinamic_add);
+    // console.log(iterator);
+    // dinamic_add = document.querySelectorAll('.dinamic_add')
+    iterator.addEventListener('click', (e) =>{
+      // console.log(e.target);
+      
+      e.preventDefault()
+      let id = iterator.getAttribute('id')
+      let input = iterator.closest('.dinamic_list_item').querySelector('input')
+      let temp = input
+
+      try {
+        var range = window.getSelection().getRangeAt(0);
+      } catch (err) {
+        // console.log(err);
+        alert('Выберите место для вставки')   
+      }
+      
+      if (range.startContainer.parentNode.classList.contains('myTxtArea_div_p')) {
+        var selectionContents = range.extractContents();
+        range.insertNode(temp);
+        dinamic_list.innerHTML = ''
+        dinamicListFunc('dinamic_add', 'Добавить') 
+      } else{
+        alert('Выберите место для вставки')
+      }
+      dinamic_list.innerHTML = ''
+      dinamicListFunc('dinamic_add', 'Добавить') 
+      
+      
+      
+      })
+      // console.log(dinamic_add);
+      
+  }
+  // dinamic_add = document.querySelectorAll('.dinamic_add')
+ 
+}
+
+
+
+
+
+myTxtAreaInput()
+
+function myTxtAreaInput() {
+  myTxtArea.addEventListener('input', () => {
+  
+    // console.log(myTxtArea_div);
+    myTxtArea_div.innerHTML = `<p class='myTxtArea_div_p'>${myTxtArea.value}</p>`
+  })
+}
+
+addDinamicClick()  
+
+
+
+function addDinamicClick(e) {
+  addDinamic = document.querySelector('.addDinamic')
+  addDinamic.addEventListener('click', (e) => {
+    e.preventDefault()
+    wrapp_dinamic_add.style.display = 'block'
+    dinamic_list.innerHTML = ''
+    dinamicListFunc('dinamic_dell', 'Удалить') 
+    dinamic_saveClick()
+  })
+}
+
+function dinamicListFunc(buttonClass, buttonValue) {
+  dinamic_div.style.display = 'block'
+
+  
+
+  if (localStorage.getItem('dinamicListArr') && localStorage.getItem('dinamicListArr').length != 2) {
+    console.log(localStorage.getItem('dinamicListArr').length);
+    console.log(localStorage.getItem('dinamicListArr'));
+    let json = localStorage.getItem('dinamicListArr')
+    dinamicListArr = JSON.parse(json)
+    
+    dinamicListArr.forEach(element => {
+      dinamicListDisplay(element, buttonClass, buttonValue)
+       });
+    dinamic_add = []
+    dinamic_add = document.querySelectorAll('.dinamic_add')
+    dinamicAddClick(dinamic_add) 
+    dinamicDellFunc()
+  }
+   else {
+    dinamic_list.innerHTML += `Динамическое поле не создано`
+  }
+}
+
+function dinamicListDisplay(element, buttonClass, buttonValue) {
+
+    dinamic_list.innerHTML += `
+    <div class='dinamic_list_item'>
+      Системное имя: ${element.name};
+      Подсказка: ${element.placeholder};
+      На странице: <input class="dinamic_input" id="${element.id}" name="${element.name}" type="text" placeholder="${element.placeholder}">
+      <button class="${buttonClass}" id="${element.id}">${buttonValue}</button>
+      <br>
+    </div>
+    `
+    
+}
+
+
+
+dinamic_div_hide.addEventListener('click', (event) =>{
+  event.preventDefault()
+  dinamic_div.style.display = 'none'
+  })
+
+let dinamic_dell
+
+function dinamic_saveClick() {
+  let dinamic_save = document.querySelector('.dinamic_save')
+
+  dinamic_save.addEventListener('click', (e) => {
+    e.preventDefault()
+    let dinamicListObj = {}
+    let id = getId(length = 16)
+  
+    let name = document.querySelector('input[name="name"]')
+    let placeholder = document.querySelector('input[name="placeholder"]')
+   
+    dinamicListObj.id = id
+    dinamicListObj.name = name.value
+    dinamicListObj.placeholder = placeholder.value
+  
+    dinamicListDisplay(dinamicListObj, 'dinamic_dell', 'Удалить')
+    
+    dinamicListArr.push(dinamicListObj)
+  
+    let json = JSON.stringify(dinamicListArr);
+    localStorage.setItem('dinamicListArr', json)
+  
+    dinamicDellFunc()
+  
+    name.value = ''
+    placeholder.value = ''
+  })
+  
+}
+
+dinamic_save.addEventListener('click', (e) => {
+  e.preventDefault()
+  let dinamicListObj = {}
+  let id = getId(length = 16)
+
+  let name = document.querySelector('input[name="name"]')
+  let placeholder = document.querySelector('input[name="placeholder"]')
+ 
+  dinamicListObj.id = id
+  dinamicListObj.name = name.value
+  dinamicListObj.placeholder = placeholder.value
+
+  dinamicListDisplay(dinamicListObj, 'dinamic_dell', 'Удалить')
+  
+  dinamicListArr.push(dinamicListObj)
+
+  let json = JSON.stringify(dinamicListArr);
+  localStorage.setItem('dinamicListArr', json)
+
+  dinamicDellFunc()
+
+  name.value = ''
+  placeholder.value = ''
+})
+
+function dinamicDellFunc() {
+  dinamic_dell = document.querySelectorAll('.dinamic_dell')
+
+  for (const iterator of dinamic_dell) {
+    if (localStorage.getItem('dinamicListArr')) {
+      dinamicListArrJson = localStorage.getItem('dinamicListArr')
+      dinamicListArr = JSON.parse(dinamicListArrJson)
+    }
+
+    iterator.addEventListener('click', (e) => {
+      e.preventDefault()
+      let id = e.target.getAttribute('id')
+      console.log(e.target.parentElement);
+      e.target.parentElement.remove()
+
+      dinamicListArr.forEach((element, index, arr) => {
+        if (element.id == id) {
+          arr.splice(index, 1)
+        }
+      });
+      let json = JSON.stringify(dinamicListArr);
+      localStorage.setItem('dinamicListArr', json)
+    })
+  }
+}
+
+
+
+
+function getId(length = 16) {
+  let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  let res = '';
+
+  for (let i = 0; i < length; i++) {
+    res += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return res;
+}
+
